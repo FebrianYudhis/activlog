625fbd9a8ed2e8b7ea785d55e3fb9557f3dec862
FebrianYudhis <killersvteam@gmail.com>
2025-03-13 14:08:26 +0700

    Versi 1.0.0

---
+++ database/migrations/2025_03_13_065139_create_date_schedules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('date_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->restrictOnDelete()->restrictOnUpdate();
+            $table->date('date');
+            $table->foreignId('schedule_id')->constrained()->restrictOnDelete()->restrictOnUpdate();
+            $table->dateTime('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('date_schedules');
+    }
+};
---
+++ database/seeders/ScheduleSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Schedule;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ScheduleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $schedules = [
+            [
+                'name' => 'Reguler 1 (Administrasi)',
+                'additional_hours' => 19,
+            ],
+            [
+                'name' => 'Reguler 2',
+                'additional_hours' => 17,
+            ],
+            [
+                'name' => 'Pagi',
+                'additional_hours' => 17,
+            ],
+            [
+                'name' => 'Pagi 2',
+                'additional_hours' => 21,
+            ],
+            [
+                'name' => 'Siang',
+                'additional_hours' => 25,
+            ],
+            [
+                'name' => 'Malam',
+                'additional_hours' => 33,
+            ],
+            [
+                'name' => 'Malam 2',
+                'additional_hours' => 33,
+            ],
+            [
+                'name' => 'Reguler Ramadan',
+                'additional_hours' => 18,
+            ],
+            [
+                'name' => 'WFA 1 (Administrasi)',
+                'additional_hours' => 19,
+            ],
+            [
+                'name' => 'WFA 2',
+                'additional_hours' => 17,
+            ]
+        ];
+        foreach ($schedules as $schedule) {
+            Schedule::create($schedule);
+        }
+    }
+}
