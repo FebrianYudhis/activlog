a5ead688426c30fe2662ce1138a69ca144ddbacc
FebrianYudhis <killersvteam@gmail.com>
2025-03-13 17:20:00 +0700

    Versi 1.0.4

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'showLoginForm']);
 
 Auth::routes();
 
