5ee31f82896cf7c6cbb2112291844d2e0e63ff84
FebrianYudhis <killersvteam@gmail.com>
2025-03-16 14:08:54 +0700

    Versi 1.0.9

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,132 +1,57 @@
-<!doctype html>
-<html lang="id">
+@extends('layouts.main')
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>{{ $judul }}</title>
+@push('css')
+<link href="https://cdn.datatables.net/v/dt/dt-2.2.2/datatables.min.css" rel="stylesheet" integrity="sha384-2vMryTPZxTZDZ3GnMBDVQV8OtmoutdrfJxnDTg0bVam9mZhi7Zr3J1+lkVFRr71f" crossorigin="anonymous">
+@endpush
 
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('assets/css/core.css') }}" />
-    <script src="{{ asset('assets/js/helpers.js') }}"></script>
-    <script src="{{ asset('assets/js/config.js') }}"></script>
-    <link href="https://cdn.datatables.net/v/dt/dt-2.2.2/datatables.min.css" rel="stylesheet" integrity="sha384-2vMryTPZxTZDZ3GnMBDVQV8OtmoutdrfJxnDTg0bVam9mZhi7Zr3J1+lkVFRr71f" crossorigin="anonymous">
-</head>
-
-<body>
-    <div class="layout-wrapper layout-content-navbar layout-without-menu">
-        <div class="layout-container">
-            <div class="layout-page">
-                <nav class="layout-navbar container-xxl navbar-detached navbar navbar-expand-xl align-items-center bg-navbar-theme" id="layout-navbar">
-                    <div class="navbar-nav-right d-flex align-items-center justify-content-end" id="navbar-collapse">
-                        <div class="navbar-nav align-items-center me-auto">
-                            <div class="nav-item d-flex align-items-center">
-                                <a href="{{ route('app') }}">Beranda</a>
-                            </div>
-                        </div>
-                        <ul class="navbar-nav flex-row align-items-center ms-md-auto">
-                            <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                                <a class="nav-link dropdown-toggle hide-arrow p-0" href="javascript:void(0);" data-bs-toggle="dropdown">
-                                    <div class="avatar avatar-online">
-                                        <img src="{{ asset('assets/avatar.png') }}" alt class="w-px-40 h-auto rounded-circle" />
-                                    </div>
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-end">
-                                    <li>
-                                        <a class="dropdown-item" href="#">
-                                            <div class="d-flex">
-                                                <div class="flex-shrink-0 me-3">
-                                                    <div class="avatar avatar-online">
-                                                        <img src="{{ asset('assets/avatar.png') }}" alt class="w-px-40 h-auto rounded-circle" />
-                                                    </div>
-                                                </div>
-                                                <div class="flex-grow-1">
-                                                    <h6 class="mb-0">{{ Auth::user()->name }}</h6>
-                                                    <small class="text-body-secondary">{{ Auth::user()->jabatan }}</small>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <div class="dropdown-divider my-1"></div>
-                                    </li>
-                                    <li>
-                                        <div class="dropdown-item">
-                                            <form action="{{ url('logout') }}" method="POST">
-                                                @csrf
-                                                <button type="submit" class="btn btn-primary w-100">Keluar</button>
-                                            </form>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-
-
-                <div class="content-wrapper">
-                    <div class="container-xxl flex-grow-1 container-p-y">
-                        <div class="layout-demo-wrapper">
-                            <div class="card">
-                                <div class="card-header">List Tanggal Logbook</div>
-                                <div class="card-body">
-                                    <table class="table table-bordered table-striped" id="tabelListTanggal">
-                                        <thead class="table-dark">
-                                            <tr>
-                                                <th>Tanggal</th>
-                                                <th>Jadwal Dinas</th>
-                                                <th>Aksi</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody></tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="content-backdrop fade"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script src="{{ asset('assets/js/jquery.js') }}"></script>
-    <script src="{{ asset('assets/js/popper.js') }}"></script>
-    <script src="{{ asset('assets/js/bootstrap.js') }}"></script>
-    <script src="https://cdn.datatables.net/v/dt/dt-2.2.2/datatables.min.js" integrity="sha384-2Ul6oqy3mEjM7dBJzKOck1Qb/mzlO+k/0BQv3D3C7u+Ri9+7OBINGa24AeOv5rgu" crossorigin="anonymous"></script>
-    <script>
-        $(document).ready(function() {
-            $('#tabelListTanggal').DataTable({
-                paging: true,
-                processing: true,
-                serverSide: true,
-                ajax: "{{ route('app') }}",
-                columns: [{
-                        data: 'date',
-                        name: 'date',
-                    },
-                    {
-                        data: 'schedule.name',
-                        name: 'schedule.name',
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: 'aksi',
-                        name: 'aksi',
-                        orderable: false,
-                        searchable: false
-                    },
-                ],
-                order: [
-                    [0, 'desc']
-                ]
-            });
+@push('js')
+<script src="https://cdn.datatables.net/v/dt/dt-2.2.2/datatables.min.js" integrity="sha384-2Ul6oqy3mEjM7dBJzKOck1Qb/mzlO+k/0BQv3D3C7u+Ri9+7OBINGa24AeOv5rgu" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function() {
+        $('#tabelListTanggal').DataTable({
+            paging: true,
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('app') }}",
+            columns: [{
+                    data: 'date',
+                    name: 'date',
+                },
+                {
+                    data: 'schedule.name',
+                    name: 'schedule.name',
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: 'aksi',
+                    name: 'aksi',
+                    orderable: false,
+                    searchable: false
+                },
+            ],
+            order: [
+                [0, 'desc']
+            ]
         });
-    </script>
-</body>
+    });
+</script>
+@endpush
 
-</html>
+@section('konten')
+<div class="card">
+    <div class="card-header">List Tanggal Logbook</div>
+    <div class="card-body">
+        <table class="table table-bordered table-striped" id="tabelListTanggal">
+            <thead class="table-dark">
+                <tr>
+                    <th>Tanggal</th>
+                    <th>Jadwal Dinas</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+    </div>
+</div>
+@endsection
