75c9ce064fb49f73da77eef29c5f4f4497d7a700
FebrianYudhis <killersvteam@gmail.com>
2025-03-17 21:24:27 +0700

    Versi 1.0.15

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Pengaturan Ulang Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah baris default yang cocok dengan alasan
+    | yang diberikan oleh broker kata sandi untuk hasil percakapan pembaruan kata sandi
+    | seperti kegagalan akibat token kata sandi / reset yang tidak valid.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah direset.',
+    'sent' => 'Kami telah mengirimkan tautan pengaturan ulang kata sandi ke email Anda.',
+    'throttled' => 'Silakan tunggu sebelum mencoba lagi.',
+    'token' => 'Token pengaturan ulang kata sandi ini tidak valid.',
+    'user' => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+
+];
